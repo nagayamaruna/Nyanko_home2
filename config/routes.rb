8b39ac1f2f895b5d27f962b98ae335137a4394e7
EC2 Default User <mooncat.rrr@gmail.com>
2024-08-04 11:43:09 +0000

    admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :admin, skip: [:registrations, :password], controllers: {
+    sessions: 'admin/sessions'
+  }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "homes#top"
   get '/home/about', to: 'homes#about'
