4be8d25ec869540c33412c91f5d7ddb63aa6787c
EC2 Default User <mooncat.rrr@gmail.com>
2024-09-05 11:31:54 +0000

    タグ検索機能

--- app/models/hashtag.rb
+++ app/models/hashtag.rb
@@ -1,2 +1,4 @@
 class Hashtag < ApplicationRecord
+  has_many :nyanko_hashtags, dependent: :destroy
+  has_many :nyankos, dependent: :destroy
 end
--- app/models/nyanko_hashtag.rb
+++ app/models/nyanko_hashtag.rb
@@ -1,3 +1,5 @@
 class NyankoHashtag < ApplicationRecord
   belongs_to :hashtag_id
+  has_many :hashtags, dependent: :destroy
+  has_many :nyankos, dependent: :destroy
 end
