b8b3cb2eae131bbcf0e8f2be3e2f1153b270edb8
EC2 Default User <mooncat.rrr@gmail.com>
2024-09-13 13:06:58 +0000

    ゲストログイン

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -1,5 +1,6 @@
 class Public::UsersController < ApplicationController
   before_action :is_matching_login_user, only: [:edit, :update]
+  before_action :ensure_guest_user, only: [:edit]
 
   def show
     @nyanko = Nyanko.new
@@ -49,6 +50,13 @@ class Public::UsersController < ApplicationController
     user = User.find(params[:id])
     unless user.id == current_user.id
       redirect_to nyankos_path
+    end
+  end
+  
+  def ensure_guest_user
+    @user = User.find(params[:id])
+    if @user.email == "guest@example.com"
+      redirect_to user_path(current_user) , notice: "ゲストユーザーはプロフィール編集画面へ遷移できません。"
     end
   end
 
